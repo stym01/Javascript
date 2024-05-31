e77d1a7775c25217c3ca301b4fc4a3a4117f7456
Satyam Kesharwani <153318865+stym01@users.noreply.github.com>
2024-05-31 06:30:09 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,12 @@
+let name="hello"
+let age=96
+console.log(`my name is ${name} and i am ${age} years old`)
+
+const gameName = new String('hitesh-hc-com') //paste in chrome and you will know everything about string
+
+let hi="    r-we-rfgr-s-s   "
+console.log(hi.trim())
+console.log(hi.replace(' ','@'))
+console.log(hi.split(' '))
+const newString = hi.substring(0, 5)
+console.log(newString)
