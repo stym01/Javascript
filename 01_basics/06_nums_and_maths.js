7444df49f92bb95679adbd221b22e1f5ec4c3bec
Satyam Kesharwani <153318865+stym01@users.noreply.github.com>
2024-05-31 07:00:06 +0000

    Maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,25 @@
+const balance = new Number(1000000.3456789);
+console.log(balance) //paste in chrome
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2)) //converts to 2 decimal places by rounding off// 100.00
+
+console.log(balance.toLocaleString()) //add comma acc to international
+//use 'en-IN' inside bracket for indian system
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
