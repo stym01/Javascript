0aeb5a37c04e87de005dfc259bc2546eac5ab9a5
Satyam Kesharwani <153318865+stym01@users.noreply.github.com>
2024-05-31 02:08:35 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,16 @@
+//let valueInNumber = Number(score)
+// convert kr dega but..."33abc" => NaN  
+//let booleanIsLoggedIn = Boolean(isLoggedIn) //to convert to boolean
+//let stringNumber = String(someNumber)
+
+//....................operations..............//
+let value =3
+let negval=-value
+console.log(negval)
+console.log("1"+2+2); //122 //agr string first hai to sb string me ho jaayega
+console.log(1+2+"2"); //32 
+console.log(+true); //1 //true+ error
+
+let num1,num2,num3
+num1=num2=num3=4
+console.table([num1,num2,num3])
