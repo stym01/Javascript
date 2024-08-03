69f3dc251cff7f6e9e8d822982481585d512191c
Satyam Kesharwani <153318865+stym01@users.noreply.github.com>
2024-08-03 10:06:21 +0000

    arr

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -3,4 +3,17 @@ const myHeors = ["shaktiman", "naagraj"]
 
 const myArr2 = new Array(1, 2, 3, 4)
 
-console.log(myArr)
+// myArr.shift() //just shift krega left me so ek element delete
+// myArr.unshift(46)  //shift towards right but 1st position will become blank hence insert the argument number
+// console.log(myArr)
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+const myNewArr=myArr.slice(1,4) //exlude last index
+console.log("B" + myNewArr) 
+console.log("A" + myArr)
+
+const myn2=myArr.splice(1,4) //include last index and take out the part from it...and hence modify the arr
+console.log("d" + myn2)
+console.log("c" + myArr)
